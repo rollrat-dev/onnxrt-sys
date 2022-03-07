6836486fe530b85d14fe213c5a6ae82212ca54a0
dracarys18 <karthihegde010@gmail.com>
2022-03-07 18:53:00 +0530

    Added Mac M1 support

--- src/generated/bindings.rs
+++ src/generated/bindings.rs
@@ -4,6 +4,9 @@ include!(concat!(env!("CARGO_MANIFEST_DIR"), "/src/generated/linux/x86_64/bindin
 #[cfg(all(target_os = "macos", target_arch = "x86_64"))]
 include!(concat!(env!("CARGO_MANIFEST_DIR"), "/src/generated/macos/x86_64/bindings.rs"));
 
+#[cfg(all(target_os = "macos", target_arch = "aarch64"))]
+include!(concat!(env!("CARGO_MANIFEST_DIR"), "/src/generated/macos/aarch64/bindings.rs"));
+
 #[cfg(all(target_os = "windows", target_arch = "x86"))]
 include!(concat!(env!("CARGO_MANIFEST_DIR"), "/src/generated/windows/x86/bindings.rs"));
 
